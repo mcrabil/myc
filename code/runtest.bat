62f9bf8dee0f870d9fc146fd23e5c57e74e73fd0
mcrabil <matthew.crabill@gmail.com>
2018-01-31 21:35:50 -0800

    iterate over all files for testing

--- code/runtest.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-i686-w64-mingw32-gcc -m32 .\mytest.s -o mytest.exe
-.\mytest.exe
-echo %ERRORLEVEL%
--- code/tests.bat
+++ code/tests.bat
@@ -1,28 +1,24 @@
 @echo off
 echo --------------
-..\Debug\myc.exe ..\stage_1\valid\multi_digit.c
-..\Debug\myc.exe ..\stage_1\valid\newlines.c
-..\Debug\myc.exe ..\stage_1\valid\no_newlines.c
-..\Debug\myc.exe ..\stage_1\valid\return_0.c
-..\Debug\myc.exe ..\stage_1\valid\return_2.c
-..\Debug\myc.exe ..\stage_1\valid\spaces.c
+FOR %%i IN (..\stage_1\valid\*) DO (
+	ECHO %%i
+	..\Debug\myc.exe %%i
+)
 echo Valid Stage 1 End
 echo --------------
-..\Debug\myc.exe ..\stage_1\invalid\missing_paren.c
-..\Debug\myc.exe ..\stage_1\invalid\missing_retval.c
-..\Debug\myc.exe ..\stage_1\invalid\no_brace.c
-..\Debug\myc.exe ..\stage_1\invalid\no_semicolon.c
-..\Debug\myc.exe ..\stage_1\invalid\no_space.c
-..\Debug\myc.exe ..\stage_1\invalid\wrong_case.c
+FOR %%i IN (..\stage_1\invalid\*) DO (
+	ECHO %%i
+	..\Debug\myc.exe %%i
+)
 echo Invalid Stage 1 End
 echo --------------
 
-..\Debug\myc.exe ..\stage_1\valid\return_0.c ..\test\return_0.s
-i686-w64-mingw32-gcc -m32 ..\test\return_0.s -o ..\test\return_0.exe
-..\test\return_0.exe
-echo %ERRORLEVEL%
+REM ..\Debug\myc.exe ..\stage_1\valid\return_0.c ..\test\return_0.s
+REM i686-w64-mingw32-gcc -m32 ..\test\return_0.s -o ..\test\return_0.exe
+REM ..\test\return_0.exe
+REM echo %ERRORLEVEL%
 
-..\Debug\myc.exe ..\stage_1\valid\return_2.c ..\test\return_2.s
-i686-w64-mingw32-gcc -m32 ..\test\return_2.s -o ..\test\return_2.exe
-..\test\return_2.exe
-echo %ERRORLEVEL%
+REM ..\Debug\myc.exe ..\stage_1\valid\return_2.c ..\test\return_2.s
+REM i686-w64-mingw32-gcc -m32 ..\test\return_2.s -o ..\test\return_2.exe
+REM ..\test\return_2.exe
+REM echo %ERRORLEVEL%
